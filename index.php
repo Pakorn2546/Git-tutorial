a24805077fd743294508866850632d02fe585f58
Pakorn2546 <titlepakornza@gmail.com>
2024-07-05 17:11:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,7 @@
     echo "<h1>My repository/h1>"
     echo "<h1>My name is is aj yai /h1>"
     echo "<h1>My name is is aj lay /h1>"
+    echo "<h1>My repository/h1>"
+    echo "<h1>My name is is aj yai /h1>"
+    echo "<h1>My name is is aj lay /h1>"
     ?>
